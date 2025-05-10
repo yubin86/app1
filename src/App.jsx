fbfdf89b3c6954979e2dac246a6bdd4b39c79679
yubin86 <yubinlee8645@gmail.com>
2025-05-10 13:23:51 -0700

    second commit-installed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='bg-black text-white'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <div className='flex flex-col min-h screen bg-[#1e1e1e] text-white'>
     <App />
+    </div>
   </StrictMode>,
 )
