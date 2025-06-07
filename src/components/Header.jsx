cf99fc29f15454128aa5cef74255d354a6c0c9ab
yubin86 <yubinlee8645@gmail.com>
2025-06-07 12:18:41 -0700

    third commit - built the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+
+    return <header>
+    <div>Yubin's blog</div>
+    <div>Blogs</div>
+    <div>About</div>
+    <div>Contact</div>
+  </header>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Header from './components/Header'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='flex flex-col min-h screen bg-[#1e1e1e] text-white'>
+    <div className='flex flex-col min-h-screen bg-[#1e1e1e] text-white'>
+    <Header/>
     <App />
     </div>
   </StrictMode>,
